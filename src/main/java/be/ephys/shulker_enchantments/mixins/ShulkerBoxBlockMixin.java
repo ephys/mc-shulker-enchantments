669f6c78a7ed1d6e614b642808f41ca693903f24
ephys <hello@guylian.me>
2021-11-25 09:43:14 +0100

    fix: remove block property injection to resolve issue with lootr
    closes #6

--- src/main/java/be/ephys/shulker_enchantments/mixins/ShulkerBoxBlockMixin.java
+++ src/main/java/be/ephys/shulker_enchantments/mixins/ShulkerBoxBlockMixin.java
@@ -1,21 +1,17 @@
 package be.ephys.shulker_enchantments.mixins;
 
-import be.ephys.shulker_enchantments.helpers.ShulkerBoxBlockHelper;
-import net.minecraft.block.*;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.ContainerBlock;
+import net.minecraft.block.ShulkerBoxBlock;
 import net.minecraft.item.BlockItem;
-import net.minecraft.item.DyeColor;
 import net.minecraft.item.ItemStack;
 import net.minecraft.loot.LootContext;
 import net.minecraft.loot.LootParameters;
 import net.minecraft.nbt.CompoundNBT;
-import net.minecraft.state.StateContainer;
 import net.minecraft.tileentity.TileEntity;
-import net.minecraft.util.Direction;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.List;
@@ -25,37 +21,6 @@ public abstract class ShulkerBoxBlockMixin extends ContainerBlock {
   protected ShulkerBoxBlockMixin(Properties builder) {
     super(builder);
   }
-
-  // ===============
-  //  Add an "Open" property that we use to select which system the block will use for rendering (static plain block VS complex model)
-  // ===============
-
-  @Inject(method = "<init>", at = @At("RETURN"))
-  public void ShulkerBoxBlockMixin$construct(DyeColor color, Properties properties, CallbackInfo ci) {
-    this.setDefaultState(
-      this.stateContainer.getBaseState()
-        .with(ShulkerBoxBlock.FACING, Direction.UP)
-        .with(ShulkerBoxBlockHelper.OPEN, false)
-    );
-  }
-
-  @Inject(method = "fillStateContainer", at = @At("RETURN"))
-  public void ShulkerBoxBlockMixin$fillStateContainer(StateContainer.Builder<Block, BlockState> builder, CallbackInfo ci) {
-    builder.add(ShulkerBoxBlockHelper.OPEN);
-  }
-
-  /**
-   * @author ephys
-   * @reason Optimising Shulker Box rendering
-   */
-  @Overwrite()
-  public BlockRenderType getRenderType(BlockState state) {
-    return state.get(ShulkerBoxBlockHelper.OPEN) ? BlockRenderType.ENTITYBLOCK_ANIMATED : BlockRenderType.MODEL;
-  }
-
-  // ===============
-  //  END
-  // ===============
 
   /**
    * Restore data persisted in PersistedItemNbt to the Shulker Box ItemStack
